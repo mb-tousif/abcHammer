c8f30c3f7df1562124631e696f3f13c6f1a42bb7
mb-tousif <meta.block.tousif@gmail.com>
2022-05-22 03:19:00 +0600

    MongoDB Connected

--- index.js
+++ index.js
@@ -8,14 +8,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_USER_PASSWORD}@abchammer.8trn1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
   try {
-
+      await client.connect();
+      const productsCollection = client.db("ABCHammer").collection("Products");
+        app.get('/products', async(req,res)=>{
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const results = await cursor.toArray();
+            console.log(results);
+            res.send(results);
+        })
 
 
   } finally {
-
-       await client.close();
+    //  await client.close();
   }
 }
 
