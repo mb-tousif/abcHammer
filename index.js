48c75d30ad1ee22ddf3c972ec823c7d29486367e
mb-tousif <meta.block.tousif@gmail.com>
2022-05-24 20:03:13 +0600

    User data post API created

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { request } = require("express");
 const uri = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_USER_PASSWORD}@abchammer.8trn1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -22,22 +23,44 @@ async function run() {
     await client.connect();
     const productsCollection = client.db("ABCHammer").collection("Products");
     const reviewsCollection = client.db("ABCHammer").collection("Reviews");
+    const usersCollection = client.db("ABCHammer").collection("Users");
 
     // Products API For Getting Data From Server!
     app.get("/products", async (req, res) => {
       const query = {};
       const cursor = productsCollection.find(query);
       const results = await cursor.toArray();
-      console.log(results);
+      // console.log(results);
       res.send(results);
     });
+
+    // Reviews API For Getting Data From Server!
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = usersCollection.find(query);
+      const results = await cursor.toArray();
+      // console.log(results);
+      res.send(results);
+    });
+
+    // POST user data when user sign up
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      const query = { email: newUser.email, name: newUser.name };
+      const exists = await usersCollection.findOne(query);
+      if(exists){
+        return res.send({success: false, newUser: exists})
+      }
+      const results = await usersCollection.insertOne(newUser);
+      res.send(request);
+    })
 
     // Reviews API For Getting Data From Server!
     app.get("/reviews", async (req, res) => {
       const query = {};
       const cursor = reviewsCollection.find(query);
       const results = await cursor.toArray();
-      console.log(results);
+      // console.log(results);
       res.send(results);
     });
     
