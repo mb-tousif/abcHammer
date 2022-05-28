0c03fc1787c7e313aa23bc8bf75a5ee13ee91f0e
mb-tousif <meta.block.tousif@gmail.com>
2022-05-28 18:46:49 +0600

    User data update APi

--- index.js
+++ index.js
@@ -56,18 +56,30 @@ async function run() {
       res.send(results);
     });
 
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    };
+
     // Make User Admin API
     // app.put("/user/admin/:email", async (req, res) => {
-    //   const email = req.params.email;
-    //     const filter = { email: email };
-    //     const updateDoc = {
-    //       $set: { role: "admin" },
-    //     };
-    //     const result = await usersCollection.updateOne(filter, updateDoc);
-    //     res.send(result);
+      // const email = req.params.email;
+      //   const filter = { email: email };
+      //   const updateDoc = {
+      //     $set: { role: "admin" },
+      //   };
+      //   const result = await usersCollection.updateOne(filter, updateDoc);
+      //   res.send(result);
      
     // });
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const requester = req.decoded.email;
       const requesterAccount = await usersCollection.findOne({
@@ -83,6 +95,19 @@ async function run() {
       } else {
         res.status(403).send({ message: "forbidden" });
       }
+    });
+
+    // POST User Data API
+    app.put("/users/:email",  async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+     const result = await usersCollection.updateOne(filter, updateDoc, options);
+     res.send(result);
     });
 
     // orders post API
